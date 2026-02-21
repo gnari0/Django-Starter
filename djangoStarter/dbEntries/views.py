cd943fd08e556fe4ede216b77a3bf83812515001
gnari0 <colbyschneiderm@gmail.com>
2026-02-21 14:15:45 -0700

    allEntries page

--- djangoStarter/dbEntries/views.py
+++ djangoStarter/dbEntries/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import dbEntry
 
 def dbEntries(request):
-    template = loader.get_template('starterPage.html')
-    return HttpResponse(template.render())
+    dbEntries = dbEntry.objects.all().values()
+    template = loader.get_template('allEntries.html')
+    context = {
+        'dbEntries': dbEntries,
+    }
+    return HttpResponse(template.render(context, request))
